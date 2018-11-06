726caa8a1cb1acf19332b31f64ac7be3f0d8eb4a
fei.wang <fei.wang@tendcloud.com>
2018-11-06 10:03:39 +0800

    代码提交

---
+++ www/config_default.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+configs = {
+    'db': {
+        'host': '172.23.4.160',
+        'port': 3306,
+        'user': 'meta',
+        'password': 'meta',
+        'database': 'wf_test'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
