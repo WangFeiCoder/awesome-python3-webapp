eb5942bd750180fa88e01f7474d698d200591396
fei.wang <fei.wang@tendcloud.com>
2018-11-06 15:12:12 +0800

    代码提交

--- www/app.py
+++ www/app.py
@@ -1,97 +1,79 @@
 # -*- coding: utf-8 -*-
+import asyncio
+import json
 import logging
-
-import asyncio, os, json, time
+import os
+import time
 from datetime import datetime
 
 from aiohttp import web
 from jinja2 import Environment, FileSystemLoader
 
+import config
 import orm
 from coroweb import add_routes, add_static
+from handlers import cookie2user, COOKIE_NAME
 
 logging.basicConfig(level=logging.INFO)
 
 
-def init_jinja2(app, **kw):
-    logging.info('init jinja2...')
-    options = dict(
-        autoescape=kw.get('autoescape', True),
-        block_start_string=kw.get('block_start_string', '{%'),
-        block_end_string=kw.get('block_end_string', '%}'),
-        variable_start_string=kw.get('variable_start_string', '{{'),
-        variable_end_string=kw.get('variable_end_string', '}}'),
-        auto_reload=kw.get('auto_reload', True)
-    )
-    path = kw.get('path', None)
-    if path is None:
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-    logging.info('set jinja2 template path: %s' % path)
-    env = Environment(loader=FileSystemLoader(path), **options)
-    filters = kw.get('filters', None)
-    if filters is not None:
-        for name, f in filters.items():
-            env.filters[name] = f
-    app['__templating__'] = env
-
-
 async def logger_factory(app, handler):
+    ' 中间件，相当于一堵墙，可以在处理请求前，对请求进行验证、筛选、记录等操作 '
+
     async def logger(request):
+        ' 记录日志 '
         logging.info('Request: %s %s' % (request.method, request.path))
-        # await asyncio.sleep(0.3)
+        ' 继续处理请求 '
         return (await handler(request))
 
     return logger
 
 
-async def data_factory(app, handler):
-    async def parse_data(request):
-        if request.method == 'POST':
-            if request.content_type.startswith('application/json'):
-                request.__data__ = await request.json()
-                logging.info('request json: %s' % str(request.__data__))
-            elif request.content_type.startswith('application/x-www-form-urlencoded'):
-                request.__data__ = await request.post()
-                logging.info('request form: %s' % str(request.__data__))
-        return (await handler(request))
-
-    return parse_data
-
-
 async def response_factory(app, handler):
     async def response(request):
-        logging.info('Response handler...')
+        logging.info('Response handler')
+        ' 对处理函数的响应进行处理 '
         r = await handler(request)
         if isinstance(r, web.StreamResponse):
+            # 处理响应流
             return r
         if isinstance(r, bytes):
+            # 处理字节类响应
             resp = web.Response(body=r)
             resp.content_type = 'application/octet-stream'
             return resp
         if isinstance(r, str):
+            # 处理字符串类响应
             if r.startswith('redirect:'):
+                # 返回重定向响应
                 return web.HTTPFound(r[9:])
             resp = web.Response(body=r.encode('utf-8'))
             resp.content_type = 'text/html;charset=utf-8'
-            return resp
+            return resp  # 返回HTML
         if isinstance(r, dict):
+            # 处理字典类响应
+            r['__user__'] = request.__user__
             template = r.get('__template__')
             if template is None:
+                # 返回JSON类响应
                 resp = web.Response(
                     body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
                 resp.content_type = 'application/json;charset=utf-8'
                 return resp
             else:
                 resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                # 获取模板，并传入响应参数进行渲染，生成HTML
                 resp.content_type = 'text/html;charset=utf-8'
                 return resp
         if isinstance(r, int) and r >= 100 and r < 600:
+            # 处理响应码
             return web.Response(r)
         if isinstance(r, tuple) and len(r) == 2:
+            # 处理有描述信息的响应码
             t, m = r
             if isinstance(t, int) and t >= 100 and t < 600:
                 return web.Response(t, str(m))
-        # default:
+        # 其他响应的返回
         resp = web.Response(body=str(r).encode('utf-8'))
         resp.content_type = 'text/plain;charset=utf-8'
         return resp
@@ -99,30 +81,71 @@ async def response_factory(app, handler):
     return response
 
 
+async def auth_factory(app, handler):
+    async def auth(request):
+        '''分析COOKIE，就登录用户绑定到requet对象'''
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = await cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+            if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+                return web.HTTPFound('/signin')
+        return (await handler(request))
+
+    return auth
+
+
+def init_jinja2(app, **kw):
+    '''模板引擎初始化'''
+    logging.info('init jinja2')
+    options = dict(
+        autoescape=kw.get('autoescape', True),  # 默认打开自动转义 转义字符
+        block_start_string=kw.get('block_start_string', '{%'),  # 模板控制块的字符串 {% block %}
+        block_end_string=kw.get('block_end_string', '%}'),
+        variable_start_string=kw.get('variable_start_string', '{{'),  # 模板变量的字符串 {{ var/func }}
+        variable_end_string=kw.get('variable_end_string', '}}'),
+        auto_reload=kw.get('auto_reload', True)
+    )
+    path = kw.get('path', None)
+    if path is None:
+        # 获得模板路径
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    logging.info('set jinja2 template path: %s' % path)
+    env = Environment(loader=FileSystemLoader(path), **options)  # 用文件系统加载器加载模板
+    filters = kw.get('filters', None)  # 尝试获取过滤器
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
+    app['__templating__'] = env  # 给Web实例程序绑定模板属性
+
+
 def datetime_filter(t):
     delta = int(time.time() - t)
     if delta < 60:
-        return u'1分钟前'
+        return u'1 minute ago'
     if delta < 3600:
-        return u'%s分钟前' % (delta // 60)
+        return u'%s minutes ago' % (delta // 60)
     if delta < 86400:
-        return u'%s小时前' % (delta // 3600)
+        return u'%s hours ago' % (delta // 3600)
     if delta < 604800:
-        return u'%s天前' % (delta // 86400)
+        return u'%s day ago' % (delta // 86400)
     dt = datetime.fromtimestamp(t)
-    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+    return u'%s-%s-%s' % (dt.year, dt.month, dt.day)
 
 
 async def init(loop):
-    await orm.create_pool(loop=loop, host='172.23.4.160', port=3306, user='meta', password='meta', db='wf_test')
-    app = web.Application(loop=loop, middlewares=[
-        logger_factory, response_factory
-    ])
+    ' 服务器运行程序：创建web实例程序，该实例程序绑定路由和处理函数，运行服务器，监听端口请求，送到路由处理 '
+    await orm.create_pool(loop=loop, **config.configs.db)
+    app = web.Application(loop=loop, middlewares=[logger_factory, auth_factory, response_factory])
     init_jinja2(app, filters=dict(datetime=datetime_filter))
     add_routes(app, 'handlers')
     add_static(app)
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
+    logging.info('Server started at http://127.0.0.1:9000')
     return srv
 
 
--- www/handlers.py
+++ www/handlers.py
@@ -8,32 +8,50 @@ import time
 
 from aiohttp import web
 
-from apis import APIValueError, APIError
-from config import configs
+import markdown2
+from apis import APIError, APIValueError, APIPermissionError, Page, APIResourceNotFoundError
 from coroweb import get, post
-from models import User, Blog, next_id
+from config import configs
+from models import User, Blog, Comment, next_id
 
 logging.basicConfig(level=logging.INFO)
-
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
 
 
+def check_admin(request):
+    if request.__user__ is None or not request.__user__.admin:
+        raise APIPermissionError()
+
+
+def get_page_index(page_str):
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError as e:
+        pass
+    if p < 1:
+        p = 1
+    return p
+
+
+def text2html(text):
+    '''将文本拼接成html格式文件'''
+    lines = map(lambda s: '<p>%s</p>' % s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;'),
+                filter(lambda s: s.strip() != '', text.split('\n')))
+    return ''.join(lines)
+
+
 def user2cookie(user, max_age):
-    '''
-    Generate cookie str by user.
-    '''
-    # build cookie string by: id-expires-sha1
-    expires = str(int(time.time() + max_age))
+    """加密cookie"""
+    expires = str(int(time.time() + max_age))  # 计算过期时间，以字符串返回
     s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
     L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
     return '-'.join(L)
 
 
 async def cookie2user(cookie_str):
-    '''
-    Parse cookie and load user if cookie is valid.
-    '''
+    '''解密cookie'''
     if not cookie_str:
         return None
     try:
@@ -42,6 +60,7 @@ async def cookie2user(cookie_str):
             return None
         uid, expires, sha1 = L
         if int(expires) < time.time():
+            # cookie过期
             return None
         user = await User.find(uid)
         if user is None:
@@ -57,93 +76,277 @@ async def cookie2user(cookie_str):
         return None
 
 
+# 用户浏览页面
+
 @get('/')
-async def index(request):
-    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    blogs = [
-        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
-        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
-        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
-    ]
+async def blogs(*, page='1'):
+    '''Blog首页'''
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    page = Page(num)
+    if num == 0:
+        blogs = []
+    else:
+        blogs = await Blog.findAll(orderBy='created_at desc', limit=(page.offset, page.limit))
     return {
         '__template__': 'blogs.html',
+        'page': page,
         'blogs': blogs
     }
 
 
 @get('/register')
-def register():
+def registerPage():
+    '''返回用户注册页面'''
     return {
         '__template__': 'register.html'
     }
 
 
 @get('/signin')
-def signin():
+def signinPage():
+    '''返回用户登录页面'''
     return {
         '__template__': 'signin.html'
     }
 
 
-@post('/api/authenticate')
-async def authenticate(*, email, passwd):
-    if not email:
-        raise APIValueError('email', 'Invalid email.')
-    if not passwd:
-        raise APIValueError('passwd', 'Invalid password.')
-    users = await User.findAll('email=?', [email])
-    if len(users) == 0:
-        raise APIValueError('email', 'Email not exist.')
-    user = users[0]
-    # check passwd:
-    sha1 = hashlib.sha1()
-    sha1.update(user.id.encode('utf-8'))
-    sha1.update(b':')
-    sha1.update(passwd.encode('utf-8'))
-    if user.passwd != sha1.hexdigest():
-        raise APIValueError('passwd', 'Invalid password.')
-    # authenticate ok, set cookie:
-    r = web.Response()
-    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
-    user.passwd = '******'
-    r.content_type = 'application/json'
-    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
-    return r
-
-
 @get('/signout')
 def signout(request):
-    referer = request.headers.get('Referer')
-    r = web.HTTPFound(referer or '/')
-    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
-    logging.info('user signed out.')
-    return r
+    '''用户退出，将SESSION中的用户信息设置为无效，返回当前页面或首页'''
+    referer = request.headers.get('Referer')  # 获取当前URL
+    resp = web.HTTPFound(referer or '/')
+    resp.set_cookie(COOKIE_NAME, '-signout-', max_age=0, httponly=True)
+    # 也可以直接清除
+    # resp.del_cookie(COOKIE_NAME)
+    logging.info('user: %s signout.' % request.__user__.name)
+    return resp
 
 
-_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
-_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+@get('/blog/{blog_id}')
+async def viewBlog(*, blog_id):
+    '''查阅一篇日志'''
+    blog = await Blog.find(blog_id)
+    comments = await Comment.findAll('blog_id=?', [blog_id], orderBy='created_at desc')
+    for comment in comments:
+        comment.html_content = text2html(comment.content)
+    blog.html_content = markdown2.markdown(blog.content)
+    return {
+        '__template__': 'blog.html',
+        'blog': blog,
+        'comments': comments
+    }
+
+
+# 管理页面
+
+@get('/manage/blogs')
+def manageBlogs(*, page='1'):
+    '''返回日志管理页'''
+    return {
+        '__template__': 'manage_blogs.html',
+        'page_index': get_page_index(page)
+    }
+
+
+@get('/manage/blogs/create')
+def manageCreateBlog():
+    '''返回日志创建页'''
+    return {
+        '__template__': 'manage_blog_edit.html',
+        'id': '',
+        'action': '/api/blogs'
+    }
+
+
+@get('/manage/blogs/edit')
+def manageEditBlog(*, id):
+    return {
+        '__template__': 'manage_blog_edit.html',
+        'id': id,
+        'action': '/api/blogs/%s' % id
+    }
+
+
+@get('/manage/users')
+def manageUsers(*, page='1'):
+    '''返回用户管理页'''
+    return {
+        '__template__': 'manage_users.html',
+        'page_index': get_page_index(page)
+    }
+
+
+@get('/manage/comments')
+def manageComments(*, page='1'):
+    '''返回评论管理页'''
+    return {
+        '__template__': 'manage_comments.html',
+        'page_index': get_page_index(page)
+    }
+
+
+# 后端API
+
+@get('/api/blogs')
+async def apiGetBlogs(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Blog.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, blogs=())
+    blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, blogs=blogs)
+
+
+@post('/api/blogs')
+async def apiCreateBlog(request, *, name, summary, content):
+    check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name', 'name cannot be empty')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'summary cannot be empty')
+    if not content or not content.strip():
+        raise APIValueError('content', 'content cannot be empty')
+    blog = Blog(user_id=request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image,
+                name=name.strip(), summary=summary.strip(), content=content.strip())
+    await blog.save()
+    return blog
+
+
+@get('/api/blogs/{id}')
+async def apiGetBlog(*, id):
+    blog = await Blog.find(id)
+    return blog
+
+
+@post('/api/blogs/{blog_id}')
+async def apiAmendBlog(blog_id, request, *, name, summary, content):
+    check_admin(request)
+    blog = await Blog.find(blog_id)
+    if not name or not name.strip():
+        raise APIValueError('name', 'name cannot be empty')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'summary cannot be empty')
+    if not content or not content.strip():
+        raise APIValueError('content', 'content cannot be empty')
+    blog.name = name.strip()
+    blog.summary = summary.strip()
+    blog.content = content.strip()
+    await blog.update()
+    return blog
+
+
+@post('/api/blogs/{blog_id}/delete')
+async def apiDeleteBlog(request, *, blog_id):
+    check_admin(request)
+    blog = await Blog.find(blog_id)
+    await blog.remove()
+    return dict(id=blog_id)
+
+
+@get('/api/comments')
+async def apiGetComments(*, page='1'):
+    page_index = get_page_index(page)
+    num = await Comment.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, comments=())
+    comments = await Comment.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    return dict(page=p, comments=comments)
+
+
+@post('/api/blogs/{blog_id}/comments')
+async def apiCreateComment(blog_id, request, *, content):
+    user = request.__user__
+    if user is None:
+        raise APIPermissionError('please signin first.')
+    if not content or not content.strip():
+        raise APIValueError('content')
+    blog = await Blog.find(blog_id)
+    if blog is None:
+        raise APIResourceNotFoundError('Blog')
+    comment = Comment(blog_id=blog.id, user_id=user.id, user_name=user.name, user_image=user.image,
+                      content=content.strip())
+    await comment.save()
+    return comment
+
+
+@post('/api/comments/{comment_id}/delete')
+async def apiDeleteComment(comment_id, request):
+    check_admin(request)
+    comment = await Comment.find(comment_id)
+    if comment is None:
+        raise APIResourceNotFoundError('Comment')
+    await comment.remove()
+    return dict(id=comment_id)
+
+
+@get('/api/users')
+async def apiGetUsers(*, page='1'):
+    page_index = get_page_index(page)
+    num = await User.findNumber('count(id)')
+    p = Page(num, page_index)
+    if num == 0:
+        return dict(page=p, users=())
+    users = await User.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+    for u in users:
+        u.passwd = '******'
+    return dict(page=p, users=users)
+
+
+_reEmail = re.compile(r'^[0-9a-z\.\-\_]+\@[0-9a-z\-\_]+(\.[0-9a-z\-\_]+){1,4}$')
+_reSha1 = re.compile(r'^[0-9a-f]{40}$')  # SHA1不够安全，后续需升级
 
 
 @post('/api/users')
-async def api_register_user(*, email, name, passwd):
-    if not name or not name.strip():
-        raise APIValueError('name')
-    if not email or not _RE_EMAIL.match(email):
-        raise APIValueError('email')
-    if not passwd or not _RE_SHA1.match(passwd):
-        raise APIValueError('passwd')
+async def apiCreateUser(*, name, email, passwd):
+    '''用户注册'''
+    # 输入验证
+    if name is None or not name.strip():
+        raise APIValueError('name', 'invalid name')
+    if email is None or not _reEmail.match(email):
+        raise APIValueError('email', 'invalid email')
+    if passwd is None or not _reSha1.match(passwd):
+        raise APIValueError('passwd', 'invalid password')
     users = await User.findAll('email=?', [email])
     if len(users) > 0:
-        raise APIError('register:failed', 'email', 'Email is already in use.')
+        raise APIError('register failed', 'email', 'Email is already in use')
+    # password 加密
     uid = next_id()
-    sha1_passwd = '%s:%s' % (uid, passwd)
-    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
-                image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
-    await user.save()
-    # make session cookie:
-    r = web.Response()
-    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
-    user.passwd = '******'
-    r.content_type = 'application/json'
-    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
-    return r
+    sha1Passwd = '%s:%s' % (uid, passwd)
+    u = User(id=uid, email=email, passwd=hashlib.sha1(sha1Passwd.encode('utf-8')).hexdigest(), name=name,
+             image='http://www.gravatar.com/avatar/%s?s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    await u.save()
+    # session
+    resp = web.Response()
+    resp.set_cookie(COOKIE_NAME, user2cookie(u, 86400), max_age=86400, httponly=True)
+    u.passwd = '******'
+    resp.content_type = 'application/json'
+    resp.body = json.dumps(u, ensure_ascii=False).encode('utf-8')
+    return resp
+
+
+@post('/api/authenticate')
+async def apiAuthenticate(*, email, passwd):
+    '''用户登录验证'''
+    # 输入验证
+    if not email:
+        raise APIValueError('email', 'invalid email')
+    if not passwd:
+        raise APIValueError('passwd', 'invalid password')
+    users = await User.findAll('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'email is not existed')
+    # password验证
+    u = users[0]
+    sha1Passwd = '%s:%s' % (u.id, passwd)
+    if u.passwd != hashlib.sha1(sha1Passwd.encode('utf-8')).hexdigest():
+        raise APIValueError('passwd', 'password is wrong')
+    # session
+    resp = web.Response()
+    resp.set_cookie(COOKIE_NAME, user2cookie(u, 86400), max_age=86400, httponly=True)
+    u.passwd = '******'
+    resp.content_type = 'application/json'
+    resp.body = json.dumps(u, ensure_ascii=False).encode('utf-8')
+    return resp
